e3d21a2b5f7e806c6a023a1201eef727375d4876
pmlzoe <pmlzoe@gmail.com>
2019-09-28 00:32:56 -0500

    Fixed indentation and cleaned up code

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Billpocket test
 
 Exercises given by Billpocket for Front-End Developer position.
-If you have any questions or (higly appreciated) feedback, hit me up at: pmlzoe@gmail.com
+If you have any questions or (highly appreciated) feedback, hit me up at: pmlzoe@gmail.com
 Thank You!
 
 ![](https://media0.giphy.com/media/KGVgi2nOVkCoE/giphy.gif)
--- Visualization/index.js
+++ Visualization/index.js
@@ -7,36 +7,31 @@
 // Other requirements:
 // - No element should be inserted if the text field is empty when the user clicks the button.
 // - Every third element in the list must be displayed in red and the remaining elements in black.
-
 const input = document.querySelector(".user-input");
 const list = document.querySelector(".list");
 const insert = document.querySelector(".ins-btn");
 let content = [];
 
 const addElement = () => {
-    (input.value.trim() !== "") ? content.push(input.value) : "";
+    (input.value.trim() !== "") ? content.push(input.value): "";
     console.log(content)
     const addLi = `<li class="el-list">${content[content.length-1]}</li>`;
-    
-    //const addLi = (input.value.trim() !== "") ? `<li class="el-list">${input.value}</li>` : "";
     list.insertAdjacentHTML("beforeend", addLi);
-    list.scrollIntoView(false)
 }
 
 window.addEventListener("keydown", e => {
-    if(e.key === "Enter") {
+    if (e.key === "Enter") {
         addElement();
-        input.value= "";
+        input.value = "";
         input.focus();
         list.scrollTop = list.scrollHeight - list.clientHeight;
-      }
+    }
 }, false)
 
 insert.addEventListener("click", e => {
     e.preventDefault();
     addElement();
-    input.value= "";
+    input.value = "";
     input.focus();
     list.scrollTop = list.scrollHeight - list.clientHeight;
 }, false)
-
